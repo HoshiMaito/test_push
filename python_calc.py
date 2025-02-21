1bb379f98f1eca88a1cace4a815d0e40c80a092e
HoshiMaito <ee33073v@nagaoka-ct.ac.jp>
2025-02-21 16:18:21 +0900

    branch change

--- python_calc.py
+++ python_calc.py
@@ -1,7 +1,10 @@
 import PySimpleGUI as sg
+import math
 
 def format_number():
-    return  float("".join(var["front"]) + "." + "".join(var["back"]) )
+    front = "".join(var["front"]) if var["front"] else "0"
+    back = "".join(var["back"]) if var["front"] else "0"
+    return  float(front + "." + back)
  
 def update_display(display_value):
     try:
@@ -25,7 +28,7 @@ def decimal_click(event):
  
 def operator_click(event):
     global var
-    if event in ["+","-","*","/","%"]:
+    if event in ["+","-","*","/","%","sin","cos","tan"]:
         var["operator"] = event
         var["x_val"] =  format_number()
         clear_click()
@@ -40,9 +43,24 @@ def clear_click():
 
 def calculate_click():
     global var
-    var["y_val"] = format_number()
-    var['result'] = eval(str(var['x_val']) + var['operator'] + str(var['y_val']))
-    update_display(var["result"]) 
+    try:
+        if var["operator"] in ["sin","cos","tan"]:
+            x = math.radians(format_number())
+            if var["operator"] == "sin":
+                var["result"] = math.sin(x)
+            elif var["operator"] == "cos":
+                var["result"] = math.cos(x)
+            elif var["operator"] == "tan":
+                var["result"] = math.tan(x)
+        
+        else:
+            var["y_val"] = format_number()
+            var['result'] = eval(str(var['x_val']) + var['operator'] + str(var['y_val']))
+
+        update_display(var["result"]) 
+    
+    except Exception as e:
+        update_display(var["Error"])
 
 
 #
@@ -51,11 +69,12 @@ def calculate_click():
 if __name__ == "__main__":
     
     layout = [[sg.Text("0.0000",key="_DISPLAY_",size=(30, 1))],
-            [sg.Button("7",key="7",size=(3, 1)),sg.Button("8",key="8",size=(3, 1)), sg.Button("9",key="9",size=(3, 1)), sg.Button("/",key="/",size=(3, 1))], 
-            [sg.Button("4",key="4",size=(3, 1)),sg.Button("5",key="5",size=(3, 1)), sg.Button("6",key="6",size=(3, 1)), sg.Button("*",key="*",size=(3, 1))],
-            [sg.Button("1",key="1",size=(3, 1)),sg.Button("2",key="2",size=(3, 1)), sg.Button("3",key="3",size=(3, 1)), sg.Button("+",key="+",size=(3, 1))],
-            [sg.Button("0",key="0",size=(3, 1)),sg.Button(".",key=".",size=(3, 1)), sg.Button("-",key="-",size=(3, 1)), sg.Button("calc",key="calc",size=(3, 1))]
-            ]
+              [sg.Button("sin",key="sin",size=(3,1)),sg.Button("cos",key="cos",size=(3,1)),sg.Button("tan",key="tan",size=(3,1)),sg.Button("C",key="C",size=(3,1))],
+              [sg.Button("7",key="7",size=(3, 1)),sg.Button("8",key="8",size=(3, 1)), sg.Button("9",key="9",size=(3, 1)), sg.Button("/",key="/",size=(3, 1))], 
+              [sg.Button("4",key="4",size=(3, 1)),sg.Button("5",key="5",size=(3, 1)), sg.Button("6",key="6",size=(3, 1)), sg.Button("*",key="*",size=(3, 1))],
+              [sg.Button("1",key="1",size=(3, 1)),sg.Button("2",key="2",size=(3, 1)), sg.Button("3",key="3",size=(3, 1)), sg.Button("+",key="+",size=(3, 1))],
+              [sg.Button("0",key="0",size=(3, 1)),sg.Button(".",key=".",size=(3, 1)), sg.Button("-",key="-",size=(3, 1)), sg.Button("calc",key="calc",size=(3, 1))]
+             ]
     window = sg.Window("簡単電卓",layout,size=(200,200), background_color="#272533",
                     return_keyboard_events=True)
     var = {"front":[],"back":[],"decimal":True,
